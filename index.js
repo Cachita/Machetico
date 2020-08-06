2060f27642c98e95a29826b956d01f9ee4130050
Anderson Bornachera <andersonb001b@gmail.com>
2020-08-06 13:00:00 -0500

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-// Machetico 
+-=-=-Machetico-=-=- 
 
-//Diferencias entre == y ===
-'El == sirve para saber si algo es verdadero o falso'
-'El === es lo mismo que == pero más estricto'
-"Ejemplo: 1==='1' es false (son iguales pero uno es un número y el otro una cadena)"
-'Ejemplo 2: 1 == 1 es true (tanto el -1 como el 1 se consideran equivalentes a un verdadero cuando se tratan como booleanos'
+Diferencias entre == y ===
+El == sirve para saber si algo es verdadero o falso
+El === es lo mismo que == pero más estricto
+Ejemplo: 1==='1' es false (son iguales pero uno es un número y el otro una cadena)
+Ejemplo 2: 1 == 1 es true (tanto el -1 como el 1 se consideran equivalentes a un verdadero cuando se tratan como booleanos
